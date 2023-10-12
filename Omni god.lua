700273c8c3792715703d17d40038cab34216d9f9
Aayan-111 <145710985+Aayan-111@users.noreply.github.com>
2023-10-12 14:02:17 -0400

    Update Omni god.lua

--- Omni god.lua
+++ Omni god.lua
@@ -1,9 +1,5 @@
 -------[[ Lets start with something simple like the Variables & Settings ]]-----
-loadstring(game:GetObjects("rbxassetid://5425999987")[1].Source)()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Reanimation.lua"))()
-game.Workspace.non.Humanoid.WalkSpeed = 45
-game.Workspace.non.Humanoid.HipHeight = 7
-
+loadstring(game:GetObjects("rbxassetid://5425999987")[1].Source)() 
 
 local char = game.Players.LocalPlayer.Character
 for i,v in pairs(char:children()) do
