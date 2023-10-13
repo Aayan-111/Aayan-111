529f130498d6e4ac4daa950b30a62d3ae3b71db5
Aayan-111 <145710985+Aayan-111@users.noreply.github.com>
2023-10-12 22:57:43 -0400

    Update Another Omni god.lua

--- Another Omni god.lua
+++ Another Omni god.lua
@@ -1,8 +1,5 @@
 -------[[ Lets start with something simple like the Variables & Settings ]]-----
 loadstring(game:GetObjects("rbxassetid://5425999987")[1].Source)()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Reanimation.lua"))()
-game.Workspace.non.Humanoid.WalkSpeed = 45
-game.Workspace.non.Humanoid.HipHeight = 7.5
 
 
 local aura = Instance.new("ParticleEmitter",game.Workspace.non.Torso)
